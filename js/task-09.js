565e3b5b6f6bd2b49fb8cc8b8ec02df658cb5ecc
Tomek Cekałą <126020134+tomcek2@users.noreply.github.com>
2023-08-09 21:26:13 +0200

    update 2

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const colourButton = document.querySelector(".change-color");
+const colourText = document.querySelector(".color");
+const colourChange = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  colourText.textContent = body.style.backgroundColor;
+};
+colourButton.addEventListener("click", colourChange);
